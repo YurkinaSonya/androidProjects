50a1b7658958cd240d6db0f7e9a196e40afb875a
YurkinaSonya <60286792+YurkinaSonya@users.noreply.github.com>
2023-04-28 14:42:46 +0700

    testOne

---
+++ app/src/main/java/com/example/mobilemodule/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mobilemodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//i change it and you can see it
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
