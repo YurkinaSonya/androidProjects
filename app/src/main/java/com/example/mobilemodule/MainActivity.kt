0b9125b99e8f15289ac440abccb812a219d672a0
YurkinaSonya <60286792+YurkinaSonya@users.noreply.github.com>
2023-04-28 14:44:30 +0700

    testTwo

--- app/src/main/java/com/example/mobilemodule/MainActivity.kt
+++ app/src/main/java/com/example/mobilemodule/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mobilemodule
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //i change it and you can see it
+//and i change it again
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
