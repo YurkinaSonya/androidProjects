c9535e10f40ae9a540922fbaf3ff4eb79d4b4804
YurkinaSonya <60286792+YurkinaSonya@users.noreply.github.com>
2023-05-05 21:49:38 +0700

    baseTest

--- app/src/main/java/com/example/mobilemodule/MainActivity.kt
+++ app/src/main/java/com/example/mobilemodule/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.mobilemodule.databinding.ActivityMainBinding
 
 //i change it and you can see it
 //and i change it again
+//and noooow
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
